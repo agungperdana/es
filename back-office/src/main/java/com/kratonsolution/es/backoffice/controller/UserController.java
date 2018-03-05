0935e756b95372af8839e2b5ee1410a0a2b32570
Agung Dodi <agung.dodi.perdana@gmail.com>
2018-03-05 22:08:17 +0700

    update users

--- back-office/src/main/java/com/kratonsolution/es/backoffice/controller/UserController.java
+++ back-office/src/main/java/com/kratonsolution/es/backoffice/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.kratonsolution.es.backoffice.controller;
 
-import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Optional;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kratonsolution.es.security.application.UserService;
+import com.kratonsolution.es.security.model.User;
+import com.kratonsolution.es.web.util.Page;
 
 /**
  * @author Agung Dodi Perdana
@@ -14,8 +21,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class UserController {
     
+    @Autowired
+    private UserService service;
+    
     @RequestMapping("/backoffice/users")
-    public String users(@AuthenticationPrincipal Optional<Principal> opt) {
+    public String users(Model model,  
+            @RequestParam("page")int page, @RequestParam("size")int size, 
+            @RequestParam(value="key", required=false) Optional<String> key) {
+        
+        Collection<User> users = new ArrayList<>();
+        
+        if(key.isPresent()) {
+            users.addAll(service.getAllUsers(key.get(), page, size));
+        }
+        else {
+            users.addAll(service.getAllUsers(page, size));
+        }
+        
+        model.addAttribute("users", users);
+        model.addAttribute("page", page+1);
+        model.addAttribute("totalPage", Page.getPage(size, users.size()));
         
         return "/backoffice/users";
     }
---
+++ web-util/src/main/java/com/kratonsolution/es/web/util/Page.java
@@ -0,0 +1,21 @@
+package com.kratonsolution.es.web.util;
+
+/**
+ * @author Agung Dodi Perdana
+ * @email agung.dodi.perdana@gmail.com 
+ */
+public class Page {
+ 
+    public static int getPage(int rowPerPage, int total) {
+        
+        int page = 1;
+        
+        if(total > rowPerPage) {
+            
+            int mod = total % rowPerPage;
+            page += (total-mod)/rowPerPage;
+        }
+        
+        return page;
+    }
+}
